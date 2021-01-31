f95341061d30f19eaa466d3a60e6051c6f59b4e6
jitendra1404 <sahjitendra731@gmail.com>
2021-01-31 06:35:23 +0545

    feedback module

--- models/customerfeedback_model.js
+++ models/customerfeedback_model.js
@@ -2,6 +2,15 @@ const mongoose =require('mongoose')
 
 const CustomerFeedback =mongoose.model('customerfeedback', {
 
+    service_name : {
+        type : String
+    },
+
+    service_discrption : {
+        type :String
+    },
+    service_
+
     
 
 })
